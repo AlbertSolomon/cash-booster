39118ecb8cd45d2001f7602cb0305ad5c6f2f5ee
AlbertSolomon <solomonaalbert@gmail.com>
2021-09-09 08:01:59 -0700

    Login and sign into firebase DB working...

--- app/src/main/java/com/example/cashbooster/Activity3.java
+++ app/src/main/java/com/example/cashbooster/Activity3.java
@@ -1,17 +1,26 @@
 package com.example.cashbooster;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Activity3 extends AppCompatActivity {
 
     Button Activity3button;
     EditText userName3, editTextPhone, passwordActivity3, passwordActivity3Confirm;
+    private FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,20 +32,58 @@ public class Activity3 extends AppCompatActivity {
         editTextPhone = (EditText)findViewById(R.id.editTextPhone);
         passwordActivity3 = (EditText)findViewById(R.id.passwordActivity3);
         passwordActivity3Confirm = (EditText)findViewById(R.id.passwordActivity3Confirm);
+        mFirebaseAuth = FirebaseAuth.getInstance();
 
-       Activity3button.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View view) {
-               openUserDashboard();
-               finish();
-           }
-       });
-    }
-    public void openUserDashboard(){
-        String username = userName3.getText().toString();
-        //String password = PasswordText.getText().toString();
 
-        Intent intent = new Intent(Activity3.this,Activity2.class);
-        startActivity(intent);
+        Activity3button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = userName3.getText().toString().trim();
+                String phoneNumber = editTextPhone.getText().toString().trim();
+                String password = passwordActivity3.getText().toString().trim();
+                String passwordConfirm = passwordActivity3Confirm.getText().toString().trim();
+
+                //openUserDashboard();
+                if (username.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Please enter your user name", Toast.LENGTH_SHORT).show();
+                }else if (phoneNumber.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Please enter in your Phone Number", Toast.LENGTH_SHORT).show();
+                }else if (password.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Please enter your Password", Toast.LENGTH_SHORT).show();
+                }
+                else if(password.length() < 5){
+                    Toast.makeText(getApplicationContext(),"Your password should contain atleast 5 characters", Toast.LENGTH_SHORT).show();
+                }
+                else if (passwordConfirm.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Please confirm your Password", Toast.LENGTH_SHORT).show();
+                }
+                else if(!password.equals(passwordConfirm)){
+                    Toast.makeText(getApplicationContext(),"Your passwords do not much", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    RegisterUser(username, password);
+                   // finish();
+                }
+
+            }
+        });
     }
+    public void RegisterUser(String username, String password){
+        mFirebaseAuth.createUserWithEmailAndPassword(username, password).addOnCompleteListener(Activity3.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull @org.jetbrains.annotations.NotNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(Activity3.this,"Login successful!", Toast.LENGTH_SHORT).show();
+
+                    startActivity(new Intent(Activity3.this, Activity2.class));
+                    finish();
+
+                }else {
+                    Toast.makeText(Activity3.this,"Login Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
 }
--- app/src/main/java/com/example/cashbooster/MainActivity.java
+++ app/src/main/java/com/example/cashbooster/MainActivity.java
@@ -5,13 +5,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.jetbrains.annotations.NotNull;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button, signupButton;
     EditText PasswordText, userName;
+
+    private FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +35,32 @@ public class MainActivity extends AppCompatActivity {
         userName=(EditText)findViewById(R.id.userName);
         PasswordText=(EditText)findViewById(R.id.PasswordText);
 
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
         //final EditText PasswordText = (EditText)findViewById(R.id.PasswordText);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openNextActivity();
-                finish();
+                String username = userName.getText().toString();
+                String password = PasswordText.getText().toString();
+
+                if (username.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Please enter your email", Toast.LENGTH_SHORT).show();
+                }
+                else if (password.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Please enter your Password", Toast.LENGTH_SHORT).show();
+                }
+                else if(password.length() < 5){
+                    Toast.makeText(getApplicationContext(),"Your password should contain atleast 5 characters", Toast.LENGTH_SHORT).show();
+                }
+                else {
+
+                    openNextActivity(username, password);
+                    //Intent intent = new Intent(MainActivity.this,Activity2.class);
+                    //startActivity(intent);
+                    //finish();
+
+                }
             }
         });
 
@@ -40,12 +72,28 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void openNextActivity(){
-        String username = userName.getText().toString();
-        String password = PasswordText.getText().toString();
+    public void openNextActivity(String username, String password){
 
-        Intent intent = new Intent(MainActivity.this,Activity2.class);
-        startActivity(intent);
+       /* mFirebaseAuth.signInWithEmailAndPassword(username,password).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(MainActivity.this,"Login successful!", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(MainActivity.this, Activity2.class));
+                }else {
+                    Toast.makeText(MainActivity.this,"Login failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });*/
+
+        mFirebaseAuth.signInWithEmailAndPassword(username,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Toast.makeText(MainActivity.this, "Login successful!",Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, Activity2.class));
+                finish();
+            }
+        });
     }
 
     public void signingUp(){
